a8417790ebe14050538c62cd4b1b250e1b9f871f
jdvalera <johnlemuelvalera@yahoo.com>
2018-01-10 13:59:01 -0800

    Initial Commit

---
+++ src/Solver.java
@@ -0,0 +1,33 @@
+
+public class Solver {
+	
+	// find a solution to the initial board (using the A* algorithm)
+	public Solver(Board initial) {
+		
+	}
+	
+	// is the initial board solvable?
+	public boolean isSolvable() {
+		return false;
+		
+	}
+	
+	// min number of moves to solve initial board; -1 if unsolvable
+	public int moves() {
+		return 0;
+		
+	}
+	
+	// sequence of boards in a shortest solution; null if unsolvable
+	public Iterable<Board> solution() {
+		return null;
+		
+	}
+	
+	// solve a slider puzzle
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
