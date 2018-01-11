78334f62f76bd493e22869d5a1ffef483893fcc4
jdvalera <johnlemuelvalera@yahoo.com>
2018-01-11 15:20:29 -0800

    Created private SearchNode class

--- src/Solver.java
+++ src/Solver.java
@@ -1,8 +1,13 @@
+import edu.princeton.cs.algs4.MinPQ;
 
 public class Solver {
 	
 	// find a solution to the initial board (using the A* algorithm)
 	public Solver(Board initial) {
+		Iterable<Board> neighbors = initial.neighbors();
+		Board predecessor = null;
+		Board searchNode = initial;
+		
 		
 	}
 	
@@ -22,6 +27,32 @@ public class Solver {
 	public Iterable<Board> solution() {
 		return null;
 		
+	}
+	
+	private class SearchNode implements Comparable<Board> {
+		
+		private SearchNode predecessor = null;
+		private Board current = null;
+		private int moves = 0;
+		private int priority = 0;
+		
+		public SearchNode(Board initial, int m, SearchNode pred) {
+			predecessor = pred;
+			moves = m;
+			current = initial;
+			
+			priority = m + initial.manhattan();
+		}
+		
+		public int getPriority() {
+			return priority;
+		}
+
+		@Override
+		public int compareTo(Board o) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
 	}
 	
 	// solve a slider puzzle
